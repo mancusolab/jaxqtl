c2d73e2b213423392508f6d188fa6765e2dcba2b
Zixuan Zhang <zzhang39@usc.edu>
2023-03-15 14:14:23 -0700

    add function to read and process count.h5ad file

--- src/jaxqtl/load/readfile.py
+++ src/jaxqtl/load/readfile.py
@@ -1,6 +1,7 @@
-from typing import NamedTuple, Optional
+from typing import NamedTuple
 
-import pandas as pd
+import decoupler as dc
+import numpy as np
 import scanpy as sc
 from anndata._core.anndata import AnnData
 from pandas_plink import read_plink1_bin
@@ -19,12 +20,66 @@ class RawDataState(NamedTuple):
     count: AnnData
 
 
-def readraw(
-    geno_path: str,
-    pheno_path: str,
-    cov_path: Optional[str],
-    min_genes: int = 200,
-    min_cells: int = 3,
+class CleanDataState(NamedTuple):
+    """
+    count: filtered cells and genes for given cell type, contains sample features
+    """
+
+    genotype: jnp.ndarray
+    count: AnnData
+
+
+def process_count(dat: AnnData, cell_type: str = "CD14-positive monocyte") -> AnnData:
+    """
+    dat: n_obs (cell) x n_vars (genes)
+    dat.var_name = 'ensembl_id'
+
+    No normalization on the count data!
+
+    ref: https://scanpy-tutorials.readthedocs.io/en/latest/pbmc3k.html
+    """
+    # start with: 11043 x 36571
+    # filter cells by min number of genes expressed (in place)
+    sc.pp.filter_cells(dat, min_genes=200)  # 11043 x 36571
+    # filter genes by min number of cells expressed (in place)
+    sc.pp.filter_genes(dat, min_cells=3)  # 11043 × 17841
+
+    # show top 20 genes highly expressed in each cell across all cells
+    # sc.pl.highest_expr_genes(dat, n_top=20, )
+    # sc.pl.scatter(dat, x='nCount_RNA', y='percent.mt')
+    # sc.pl.scatter(dat, x='nCount_RNA', y='nFeature_RNA')
+
+    #  filter cells with too many genes expressed
+    dat = dat[dat.obs["nFeature_RNA"] < 2500, :]  # 11026 × 17841
+
+    #  filter cells that have >5% mitochondrial counts
+    dat = dat[dat.obs["percent.mt"] < 5, :]  # 10545 × 17841
+
+    # normalize by total UMI count: scale factor (in-place change), CPM if target=1e6
+    sc.pp.normalize_total(dat, target_sum=1e4)
+
+    # mean count for given cell type within individual
+    dat.bulk = dc.get_pseudobulk(
+        dat,
+        sample_col="donor_id",
+        groups_col="cell_type",
+        mode="mean",
+        min_prop=0.2,
+        min_cells=0,
+        min_counts=0,
+        min_smpls=2,
+    )
+
+    # subset to one cell type
+    dat_onetype = dat.bulk[dat.bulk.obs["cell_type"] == cell_type]
+    # filter out genes with zeros reads
+    dat_onetype = dat_onetype[:, np.sum(dat_onetype.X, axis=0) > 0]
+
+    return dat_onetype
+
+
+def read_data(
+    geno_path: str, pheno_path: str, cell_type: str = "CD14-positive monocyte"
 ):
     """
     Genotype data: plink file
@@ -34,7 +89,6 @@ def readraw(
     - dat.X: cell x gene sparse matrix, where cell is indexed by unique barcode
     - dat.obs: cell x features (eg. donor_id, age,...)
     - dat.var: gene x gene summary stats
-    -
 
     recode sex as: female = 1, male = 0
     """
@@ -43,27 +97,14 @@ def readraw(
     bim_path = geno_path + ".bim"
     fam_path = geno_path + ".fam"
 
-    G = read_plink1_bin(bed_path, bim_path, fam_path, verbose=False)
-    genotype = jnp.array(G.values)  # sample x variants
-
-    # read age, sex and PCs from plink PCA result
-    cov = pd.read_csv(cov_path, sep=" ", header=None)
-    cov = jnp.array(cov)[:, 2:]
-
-    dat = sc.read_h5ad(pheno_path)
-
-    # pre-process steps: no normalization
-    # https://scanpy-tutorials.readthedocs.io/en/latest/pbmc3k.html
-    # for each gene in a given cell, take mean of
-
-    # filter cells by min number of genes expressed (in place)
-    sc.pp.filter_cells(dat, min_genes=min_genes)
-    # filter genes by min number of cells expressed (in place)
-    sc.pp.filter_genes(dat, min_cells=min_cells)
-    # filter by mitochondria
-
     # a0=0, a1=1, genotype value (0/1/2) is the count for a1 allele
     # print(G.a0.sel(variant="variant0").values)
     # print(G.sel(sample="1", variant="variant0").values)
+    G = read_plink1_bin(bed_path, bim_path, fam_path, verbose=False)
+    genotype = jnp.array(G.values)  # sample x variants
 
-    return RawDataState(genotype, dat)
+    dat = sc.read_h5ad(pheno_path)
+    dat.obs["sex"] = np.where(dat.obs["sex"] == "female", 1, 0)
+    count = process_count(dat, cell_type=cell_type)  # count and covariates
+
+    return CleanDataState(genotype, count)
--- tests/test_readfile.py
+++ tests/test_readfile.py
@@ -2,22 +2,19 @@ import numpy.testing as nptest
 
 from jax.config import config
 
-from jaxqtl.load.readfile import readraw
+from jaxqtl.load.readfile import read_data
 
 config.update("jax_enable_x64", True)
 
 geno_path = "./tests/data/onek1k"
 pheno_path = "./tests/data/Countdata.h5ad"
-cov_path = "./tests/data/onek1kpca.eigenvec"
 
 
 # Check shape of data loaded, i.e. sample size is the same
 def assert_sampleN_eq(Data, rtol=1e-10):
-    nptest.assert_allclose(
-        Data.genotype.shape[0], len(Data.count.obs.donor_id.unique()), rtol=rtol
-    )
+    nptest.assert_allclose(Data.genotype.shape[0], Data.count.X.shape[0], rtol=rtol)
 
 
-def test_readraw():
-    RawData = readraw(geno_path, pheno_path, cov_path)
-    return assert_sampleN_eq(RawData)
+def test_read_data():
+    dat = read_data(geno_path, pheno_path)
+    return assert_sampleN_eq(dat)
