fix bug in score calculation (take big inv), need score test for one additional covar g

src/jaxqtl/infer/glm.py
@@ -98,19 +98,17 @@ class GLM:
         family: ExponentialFamily,
         X: ArrayLike,
         y: ArrayLike,
-        mu_null: ArrayLike,
-        eta_null: ArrayLike,
+        glm_null_res: GLMState,
         df: float,
     ) -> Array:
         """test for additional covariate g
         X is the full design matrix containing covariates and g
         calculate score in full model using the model fitted from null model
         """
-        score_null = family.score(X, y, mu_null)
-        _, _, weight = family.calc_weight(X, y, eta_null)
-        score_null_info = (X * weight).T @ X
-        TS = (score_null[-1] ** 2) / jnp.diag(score_null_info)[-1]  # get TS for g
-        pval = 1 - chi2.cdf(TS, df)
+        score_null = family.score(X, y, glm_null_res.mu)
+        score_null_info = (X * glm_null_res.glm_wt).T @ X
+        TS_chi2 = score_null.T @ jnpla.inv(score_null_info) @ score_null
+        pval = 1 - chi2.cdf(TS_chi2, df)
         return pval
 
     def sumstats(self, weight) -> Tuple[Array, Array]:

tests/test_infer.py
@@ -24,6 +24,35 @@ maxiter = 100
 
 test_resid_family = Binomial()  # Poisson reg result is closer
 
+# ###
+# mod_full = GLM(
+#     X=spector_data.exog,
+#     y=spector_data.endog,
+#     family=Poisson(),
+#     append=False,
+#     maxiter=maxiter,
+#     stepsize=stepsize,
+# ).fit()
+# print(mod_full.p[-1])
+#
+# mod_null = GLM(
+#     X=spector_data.exog.drop("PSI", axis=1),
+#     y=spector_data.endog,
+#     family=Poisson(),
+#     append=False,
+#     maxiter=maxiter,
+#     stepsize=stepsize,
+# ).fit()
+#
+# pval_score = GLM.score_test_add_g(
+#     Poisson(),
+#     jnp.array(spector_data.exog),
+#     jnp.array(spector_data.endog)[:, jnp.newaxis],
+#     mod_null,
+#     1.0,
+# )
+# ###
+
 
 def test_resid_reg():
     X = spector_data.exog.copy()
@@ -216,12 +245,11 @@ def test_poisson_scoretest():
         Poisson(),
         jnp.array(spector_data.exog),
         jnp.array(spector_data.endog)[:, jnp.newaxis],
-        mod_null.mu,
-        mod_null.eta,
+        mod_null,
         1.0,
     )
 
-    # not sure  if the discrepancy is caused by small sample size n=30
+    # the discrepancy might be caused by small sample size n=30
     assert_array_eq(pval_score, mod_full.p[-1])
 
 