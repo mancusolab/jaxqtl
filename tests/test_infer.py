43f247caf210af7aeb839fe0505d55a905c9c729
Zixuan Zhang <zzhang39@usc.edu>
2023-08-17 11:01:06 -0700

    clean up solve and test_infer

--- tests/test_infer.py
+++ tests/test_infer.py
@@ -13,7 +13,7 @@ from jax.config import config
 
 from jaxqtl.families.distribution import Binomial, Poisson
 from jaxqtl.infer.glm import GLM
-from jaxqtl.infer.solve import CGSolve, CholeskySolve
+from jaxqtl.infer.solve import CGSolve, CholeskySolve, QRSolve
 
 config.update("jax_enable_x64", True)
 
@@ -21,28 +21,242 @@ config.update("jax_enable_x64", True)
 spector_data = sm.datasets.spector.load()
 spector_data.exog = sm.add_constant(spector_data.exog, prepend=True)  # X
 
+y_arr = jnp.array(spector_data.endog)[:, jnp.newaxis]
+X_arr = jnp.array(spector_data.exog)
+
 maxiter = 100
 stepsize = 1.0
 
 
+def test_linear_regression_cho():
+
+    # test linear regression function
+    mod = sm.OLS(spector_data.endog, spector_data.exog)
+    sm_state = mod.fit()
+
+    jaxqtl_cho = GLM(
+        solver=CholeskySolve(),
+        maxiter=maxiter,
+        stepsize=stepsize,
+    )
+
+    init_lm = jaxqtl_cho.family.init_eta(jnp.array(spector_data.endog)[:, jnp.newaxis])
+
+    glm_state = jaxqtl_cho.fit(X_arr, y_arr, init=init_lm)
+
+    assert_betas_eq(glm_state, sm_state)
+    assert_array_eq(glm_state.se, sm_state.bse)
+    assert_array_eq(glm_state.p, sm_state.pvalues)
+
+
+def test_binomial_cg():
+    # test logistic regression
+    mod = sm.Logit(spector_data.endog, spector_data.exog)
+    sm_state = mod.fit()
+
+    jaxqtl_bin_cg = GLM(
+        family=Binomial(),
+        maxiter=maxiter,
+        solver=CGSolve(),
+        stepsize=stepsize,
+    )
+    init_logistic = jaxqtl_bin_cg.family.init_eta(y_arr)
+    glm_state = jaxqtl_bin_cg.fit(X_arr, y_arr, init=init_logistic)
+
+    assert_betas_eq(glm_state, sm_state, rtol=1e-4)
+    assert_array_eq(glm_state.se, sm_state.bse, rtol=1e-4)
+    assert_array_eq(glm_state.p, sm_state.pvalues, rtol=1e-4)
+
+
+def test_binomial_cho():
+    # test logistic regression
+    mod = sm.Logit(spector_data.endog, spector_data.exog)
+    sm_state = mod.fit()
+
+    jaxqtl_bin_cho = GLM(
+        family=Binomial(),
+        maxiter=maxiter,
+        solver=CholeskySolve(),
+        stepsize=stepsize,
+    )
+    init_logistic = jaxqtl_bin_cho.family.init_eta(y_arr)
+    glm_state = jaxqtl_bin_cho.fit(X_arr, y_arr, init=init_logistic)
+
+    assert_betas_eq(glm_state, sm_state, rtol=1e-4)
+    assert_array_eq(glm_state.se, sm_state.bse, rtol=1e-4)
+    assert_array_eq(glm_state.p, sm_state.pvalues, rtol=1e-4)
+
+
+def test_poisson_qr():
+    # test logistic regression
+    mod = smPoisson(spector_data.endog, spector_data.exog)
+    sm_state = mod.fit()
+
+    jaxqtl_poisson_qr = GLM(
+        family=Poisson(),
+        maxiter=maxiter,
+        solver=QRSolve(),
+        stepsize=stepsize,
+    )
+    init_pois = jaxqtl_poisson_qr.family.init_eta(y_arr)
+    glm_state = jaxqtl_poisson_qr.fit(X_arr, y_arr, init=init_pois)
+
+    assert_betas_eq(glm_state, sm_state)
+    assert_array_eq(glm_state.se, sm_state.bse)
+    assert_array_eq(glm_state.p, sm_state.pvalues)
+
+
+def test_poisson_cho():
+    # test logistic regression
+    mod = smPoisson(spector_data.endog, spector_data.exog)
+    sm_state = mod.fit()
+
+    jaxqtl_poisson_cho = GLM(
+        family=Poisson(),
+        maxiter=maxiter,
+        solver=CholeskySolve(),
+        stepsize=stepsize,
+    )
+    init_pois = jaxqtl_poisson_cho.family.init_eta(y_arr)
+    glm_state = jaxqtl_poisson_cho.fit(X_arr, y_arr, init=init_pois)
+
+    assert_betas_eq(glm_state, sm_state)
+    assert_array_eq(glm_state.se, sm_state.bse)
+    assert_array_eq(glm_state.p, sm_state.pvalues)
+
+
+def test_poisson_cg():
+    # test logistic regression
+    mod = smPoisson(spector_data.endog, spector_data.exog)
+    sm_state = mod.fit()
+
+    jaxqtl_poisson_cg = GLM(
+        family=Poisson(),
+        maxiter=maxiter,
+        solver=CGSolve(),
+        stepsize=stepsize,
+    )
+    init_pois = jaxqtl_poisson_cg.family.init_eta(y_arr)
+    glm_state = jaxqtl_poisson_cg.fit(X_arr, y_arr, init=init_pois)
+
+    assert_betas_eq(glm_state, sm_state)
+    assert_array_eq(glm_state.se, sm_state.bse)
+    assert_array_eq(glm_state.p, sm_state.pvalues)
+
+
+def test_CGsolve_realdata():
+    """
+    # AssertionError: get diff result
+    """
+    dat = jnp.array(
+        pd.read_csv("./example/data/ENSG00000178607_rs74787440.gz", sep="\t")
+    )
+    y = dat[:, -2][:, jnp.newaxis]
+    X = dat[:, 0:-2]
+
+    sm_state = smPoisson(np.array(y), np.array(X)).fit()
+
+    jaxqtl_poisson_cg = GLM(
+        family=Poisson(), maxiter=maxiter, solver=CGSolve(), stepsize=stepsize
+    )
+    init_pois = jaxqtl_poisson_cg.family.init_eta(y)
+    glm_state = jaxqtl_poisson_cg.fit(X, y, init=init_pois)
+
+    assert_betas_eq(glm_state, sm_state)
+    assert_array_eq(glm_state.se, sm_state.bse)
+    assert_array_eq(glm_state.p, sm_state.pvalues)
+
+
+def test_1D_X():
+    # test poisson regression
+    mod = smPoisson(spector_data.endog, spector_data.exog["PSI"])
+    sm_state = mod.fit()
+
+    X_arr = jnp.array(spector_data.exog["PSI"])[:, jnp.newaxis]
+    y_arr = jnp.array(spector_data.endog)[:, jnp.newaxis]
+
+    jaxqtl_pois = GLM(family=Poisson(), maxiter=maxiter, stepsize=stepsize)
+    init_pois = jaxqtl_pois.family.init_eta(y_arr)
+    glm_state = jaxqtl_pois.fit(X_arr, y_arr, init=init_pois)
+
+    assert_betas_eq(glm_state, sm_state)
+    assert_array_eq(glm_state.se, sm_state.bse)
+    assert_array_eq(glm_state.p, sm_state.pvalues)
+
+
+def test_sandwich():
+    """
+    Compare sandwitch estimator from stats model to jaxqtl
+    """
+    dat = pd.read_csv("./example/data/ENSG00000178607_rs74787440.gz", sep="\t")
+    M = jnp.array(dat.iloc[:, 0:12])
+    y = jnp.array(dat["y"])[:, jnp.newaxis]
+    library_size = jnp.array(dat["log_offset"])[:, jnp.newaxis]
+
+    sm_mod = sm.GLM(
+        np.array(y),
+        np.array(M),
+        family=sm.families.Poisson(),
+        offset=np.array(library_size).reshape((len(library_size),)),
+    ).fit()
+    white_cov = statsmodels.stats.sandwich_covariance.cov_white_simple(
+        sm_mod, use_correction=False
+    )
+
+    jaxqtl_pois = GLM(family=Poisson(), maxiter=100, solver=CholeskySolve())
+    init_pois = jaxqtl_pois.family.init_eta(y)
+
+    glmstate = jaxqtl_pois.fit(
+        M, y, init=init_pois, offset_eta=library_size, robust_se=True
+    )
+
+    assert_array_eq(glmstate.se ** 2, jnp.diag(white_cov))
+
+
+def test_poisson_scoretest():
+    jaxqtl_pois = GLM(family=Poisson(), maxiter=maxiter, stepsize=stepsize)
+    init_pois = jaxqtl_pois.family.init_eta(y_arr)
+    mod_full = jaxqtl_pois.fit(X_arr, y_arr, init=init_pois)
+    print(mod_full.p[-1])
+
+    mod_null = jaxqtl_pois.fit(
+        jnp.array(spector_data.exog.drop("GPA", axis=1)), y_arr, init=init_pois
+    )
+
+    pval_score = GLM.score_test_add_g(
+        Poisson(),
+        X_arr,
+        y_arr,
+        mod_null,
+        1.0,
+    )
+
+    # the discrepancy might be due to small sample size n=30
+    assert_array_eq(pval_score, mod_full.p[-1])
+
+
 def test_resid_reg():
-    test_resid_family = Poisson()  # Poisson reg result is closer
+    """
+    project out covariates first;
+    results are wrong
+    """
+    test_resid_family = Binomial()  # Poisson reg result is closer
 
     X = spector_data.exog.copy()
     y = jnp.array(spector_data.endog)[:, jnp.newaxis]
 
-    truth = GLM(
+    jaxqtl_pois = GLM(
         family=test_resid_family,
         maxiter=maxiter,
         stepsize=stepsize,
-    ).fit(jnp.array(X), y)
+        solver=CholeskySolve(),
+    )
+    init_pois = jaxqtl_pois.family.init_eta(y)
+    truth = jaxqtl_pois.fit(jnp.array(X), y, init=init_pois)
 
     covar = X.drop("PSI", axis=1)
 
-    glmstate_null = GLM(
-        family=test_resid_family,
-        maxiter=100,
-    ).fit(jnp.array(covar), y)
+    glmstate_null = jaxqtl_pois.fit(jnp.array(covar), y, init=init_pois)
 
     # check this: https://github.com/rgcgithub/regenie/blob/master/src/Step2_Models.cpp
     PSI = jnp.array(X["PSI"])
@@ -57,10 +271,12 @@ def test_resid_reg():
         - projection_covar @ PSI[:, jnp.newaxis]
     )
 
-    glmstate = GLM(
-        family=test_resid_family,
-        maxiter=100,
-    ).fit(jnp.array(X["PSI_resid"])[:, jnp.newaxis], y, glmstate_null.eta)
+    glmstate = jaxqtl_pois.fit(
+        jnp.array(X["PSI_resid"])[:, jnp.newaxis],
+        y,
+        offset_eta=glmstate_null.eta,
+        init=init_pois,
+    )
 
     print(f"betas: truth={truth.beta[-1]}, est={glmstate.beta}")
     print(f"SE: truth={truth.se[-1]}, est={glmstate.se}")
@@ -89,196 +305,3 @@ def test_resid_reg():
 
     assert_array_eq(glmstate.beta, truth.beta[-1])
     assert_array_eq(glmstate.se, truth.se[-1])
-
-
-def test_linear_regression():
-
-    # test linear regression function
-    mod = sm.OLS(spector_data.endog, spector_data.exog)
-    sm_state = mod.fit()
-
-    test_irls = GLM(
-        maxiter=maxiter,
-        stepsize=stepsize,
-    )
-
-    glm_state = test_irls.fit(
-        jnp.array(spector_data.exog), jnp.array(spector_data.endog)[:, jnp.newaxis]
-    )
-    assert_betas_eq(glm_state, sm_state)
-
-    test_irls = GLM(
-        solver=CholeskySolve(),
-        maxiter=maxiter,
-        stepsize=stepsize,
-    )
-    glm_state = test_irls.fit(
-        jnp.array(spector_data.exog), jnp.array(spector_data.endog)[:, jnp.newaxis]
-    )
-
-    assert_betas_eq(glm_state, sm_state)
-    assert_array_eq(glm_state.se, sm_state.bse)
-    assert_array_eq(glm_state.p, sm_state.pvalues)
-
-
-def test_logistic():
-    # test logistic regression
-    mod = sm.Logit(spector_data.endog, spector_data.exog)
-    sm_state = mod.fit()
-
-    test_logit = GLM(
-        family=Binomial(),
-        maxiter=maxiter,
-        solver=CGSolve(),
-        stepsize=stepsize,
-    )
-    glm_state = test_logit.fit(
-        jnp.array(spector_data.exog), jnp.array(spector_data.endog)[:, jnp.newaxis]
-    )
-    assert_betas_eq(glm_state, sm_state, rtol=1e-4)
-    assert_array_eq(glm_state.se, sm_state.bse, rtol=1e-4)
-    assert_array_eq(glm_state.p, sm_state.pvalues, rtol=1e-4)
-
-
-def test_poisson():
-    # test logistic regression
-    mod = smPoisson(spector_data.endog, spector_data.exog)
-    sm_state = mod.fit()
-
-    test_poisson = GLM(
-        family=Poisson(),
-        maxiter=maxiter,
-        stepsize=stepsize,
-    )
-    glm_state = test_poisson.fit(
-        jnp.array(spector_data.exog), jnp.array(spector_data.endog)[:, jnp.newaxis]
-    )
-    assert_betas_eq(glm_state, sm_state)
-    assert_array_eq(glm_state.se, sm_state.bse)
-    assert_array_eq(glm_state.p, sm_state.pvalues)
-
-
-def test_1D_X():
-    # test poisson regression
-    mod = smPoisson(spector_data.endog, spector_data.exog["PSI"])
-    sm_state = mod.fit(disp=0)
-
-    test_poisson = GLM(
-        family=Poisson(),
-        maxiter=maxiter,
-        stepsize=stepsize,
-    )
-    glm_state = test_poisson.fit(
-        jnp.array(spector_data.exog["PSI"])[:, jnp.newaxis],
-        jnp.array(spector_data.endog)[:, jnp.newaxis],
-    )
-    assert_betas_eq(glm_state, sm_state)
-    assert_array_eq(glm_state.se, sm_state.bse)
-    assert_array_eq(glm_state.p, sm_state.pvalues)
-
-
-def test_CGsolve():
-    dat = jnp.array(
-        pd.read_csv("./example/data/ENSG00000178607_rs74787440.gz", sep="\t")
-    )
-    y = dat[:, -2][:, jnp.newaxis]
-    X = dat[:, 0:-2]
-
-    sm_state = smPoisson(np.array(y), np.array(X)).fit(disp=0)
-
-    glm_state = GLM(
-        family=Poisson(),
-        solver=CGSolve(),
-        maxiter=maxiter,
-        stepsize=stepsize,
-    ).fit(X, y)
-    assert_betas_eq(glm_state, sm_state)
-    assert_array_eq(glm_state.se, sm_state.bse)
-    assert_array_eq(glm_state.p, sm_state.pvalues)
-
-
-def test_poisson_scoretest():
-    mod_full = GLM(
-        family=Poisson(),
-        maxiter=maxiter,
-        stepsize=stepsize,
-    ).fit(jnp.array(spector_data.exog), jnp.array(spector_data.endog)[:, jnp.newaxis])
-    print(mod_full.p[-1])
-
-    mod_null = GLM(family=Poisson(), maxiter=maxiter, stepsize=stepsize,).fit(
-        jnp.array(spector_data.exog.drop("GPA", axis=1)),
-        jnp.array(spector_data.endog)[:, jnp.newaxis],
-    )
-
-    pval_score = GLM.score_test_add_g(
-        Poisson(),
-        jnp.array(spector_data.exog),
-        jnp.array(spector_data.endog)[:, jnp.newaxis],
-        mod_null,
-        1.0,
-    )
-
-    # the discrepancy might be caused by small sample size n=30
-    assert_array_eq(pval_score, mod_full.p[-1])
-
-
-def test_sandwich():
-    dat = pd.read_csv("./example/data/ENSG00000178607_rs74787440.gz", sep="\t")
-    M = jnp.array(dat.iloc[:, 0:12])
-    y = jnp.array(dat["y"])[:, jnp.newaxis]
-    library_size = jnp.array(dat["log_offset"])[:, jnp.newaxis]
-
-    sm_mod = sm.GLM(
-        np.array(y),
-        np.array(M),
-        family=sm.families.Poisson(),
-        offset=np.array(library_size).reshape((len(library_size),)),
-    ).fit()
-    white_cov = statsmodels.stats.sandwich_covariance.cov_white_simple(
-        sm_mod, use_correction=False
-    )
-
-    # full model fit to compare Wald p to Score p
-    glmstate = GLM(family=Poisson(), maxiter=100, solver=CholeskySolve()).fit(
-        M, y, offset_eta=library_size, robust_se=True
-    )
-
-    assert_array_eq(glmstate.se ** 2, jnp.diag(white_cov))
-
-
-# -------------------------------------------------#
-
-# data = sm.datasets.scotland.io()
-# data.exog = sm.add_constant(data.exog)
-# gamma_model = sm.GLM(data.endog, data.exog, family=sm.families.Gamma())
-# gamma_results = gamma_model.fit()
-# print(gamma_results.summary())
-
-# test_Gamma = GLM(
-#     X=data.exog,
-#     y=data.endog,
-#     family="Gamma",
-#     seed=123,
-#     solver=solver,
-#     append=False,
-# )
-# test_Gamma.fit()
-# print(test_Gamma)
-
-# data = sm.datasets.scotland.io()
-# data.exog = sm.add_constant(data.exog)
-# NB_model = NegativeBinomial(spector_data.endog, spector_data.exog)
-# NB_results = NB_model.fit(maxiter=100)
-# print(NB_results.summary())
-
-# test_NB = GLM(
-#     X=data.exog,
-#     y=data.endog,
-#     family="NB",
-#     seed=123,
-#     solver=solver,
-#     append=False,
-#     init="default",
-# )
-# test_NB.fit()
-# print(test_NB)
