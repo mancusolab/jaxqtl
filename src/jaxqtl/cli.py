494e2994a448801ad0614c043f8db261e703caf7
zixuan zhang <zzhang39@usc.edu>
2025-06-20 08:54:07 -0700

    debug x64 config for tpu

--- src/jaxqtl/cli.py
+++ src/jaxqtl/cli.py
@@ -295,8 +295,10 @@ def main(args):
     args = argp.parse_args(args)  # a list a strings
 
     jax.config.update("jax_platform_name", args.platform)
-    if args.platform != "tpu":
+    if args.platform == "tpu":
         # TPU not support complex 64, only 16 and 32
+        jax.config.update("jax_enable_x64", False)
+    else:
         jax.config.update("jax_enable_x64", True)
 
     log = get_logger(__name__, args.out)
